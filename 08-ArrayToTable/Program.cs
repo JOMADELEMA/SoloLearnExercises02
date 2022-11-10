5c24fe037077580fae5f8a2276a232efc0d238e0
JOMADELEMA <jomadelema@gmail.com>
2022-11-10 13:00:10 -0600

    Created and solved 08-ArrayToTable

---
+++ 08-ArrayToTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_ArrayToTable
+{
+    internal class Program
+    {
+        /*
+         * Complete the program to output this array in the form of a table (without separation):
+         * 123
+         * 456
+         * 789
+         * 
+         */
+        static void Main(string[] args)
+        {
+            int[,] num = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int i=0; i<3; i++)
+            {
+                for(int j=0; j<3; j++)
+                {
+                    Console.Write(num[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
