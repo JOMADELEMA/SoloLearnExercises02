683b00eda3fd25d70254adfe6a367a2dee2a2883
JOMADELEMA <jomadelema@gmail.com>
2022-11-10 07:09:32 -0600

    Solved WelcomeMessage

--- 01 - Welcome/Program.cs
+++ 01 - Welcome/Program.cs
@@ -8,8 +8,22 @@ namespace _01___Welcome
 {
     internal class Program
     {
+        /*
+         * Define a class Welcome which has one public method
+         * called WelcomeMessage(), and should print "Welcome to OOP" when called. 
+         */
         static void Main(string[] args)
         {
+            Welcome welcome = new Welcome();
+            welcome.WelcomeMessage();
+        }
+
+        public class Welcome
+        {
+            public void WelcomeMessage()
+            {
+                Console.WriteLine("Welcome to OOP");
+            }
         }
     }
 }
