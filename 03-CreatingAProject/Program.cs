360da0867a04e272db26987f8f8028c5b4fb49fa
JOMADELEMA <jomadelema@gmail.com>
2022-11-10 07:34:11 -0600

    Created and solved 03-CreatingAProject

---
+++ 03-CreatingAProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_CreatingAProject
+{
+    internal class Program
+    {
+        /*
+         * Your graphic application needs to report that a new 
+         * project has been created successfully once the "Create"
+         * button has been pressed. 
+         * 
+         * Complete the given class by adding a constructor that will show 
+         * message "Prject created" once the operation is done. 
+         */
+        static void Main(string[] args)
+        {
+            Project project = new Project();
+
+        }
+
+        class Project
+        {
+            public Project()
+            {
+                Console.WriteLine("Project created");
+            }
+        }
+    }
+}
