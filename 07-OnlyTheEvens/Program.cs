f5176382ed72001ba8eed5ed1a91ddd8729f6dde
JOMADELEMA <jomadelema@gmail.com>
2022-11-10 12:44:17 -0600

    Created and solved 07-OnlyTheEvens

---
+++ 07-OnlyTheEvens/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_OnlyTheEvens
+{
+    internal class Program
+    {
+        /*
+         * The program you are given takes 5 numbers as input
+         * and stores them in an array. 
+         * Complete the program to go through the array and 
+         * output the sume of even numbers. 
+         * 
+         * sample input
+         * 10
+         * 890
+         * 15
+         * 3699
+         * 14
+         * 
+         * sample output 
+         * 914
+         */
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            int count = 0;
+
+            while (count < 5)
+            {
+                numbers[count] = Convert.ToInt32(Console.ReadLine());
+                count++;
+            }
+
+            int sum = 0;
+            foreach(var number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
