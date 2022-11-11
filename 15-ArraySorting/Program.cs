ff19b0705f197f3efbcf1d691636482173605602
JOMADELEMA <jomadelema@gmail.com>
2022-11-10 21:57:56 -0600

    Created and solved 15-ArraySorting

---
+++ 15-ArraySorting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_ArraySorting
+{
+    internal class Program
+    {
+        /*
+         * The program you are given takes the N number as the
+         * size of an array, followed by N numbers. 
+         * Complete the program to sort and output every 
+         * element of an array, each on a new line. 
+         * 
+         * Sample input
+         * 4
+         * 1
+         * 114
+         * 3
+         * 5
+         * 
+         * Sample output
+         * 1
+         * 3
+         * 5
+         * 14
+         * 
+         */
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[count];
+
+            for(int i=0; i < count; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
